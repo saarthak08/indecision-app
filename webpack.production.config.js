6069cc5abd56335c2272caaee3d6fe86d1dbbd27
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-05-16 12:46:30 +0530

    refactor

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -28,19 +28,14 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     {
-                        // We configure 'MiniCssExtractPlugin'              
                         loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: 'css-loader',
                         options: {
                             modules: true,
-                            // Allows to configure how many loaders 
-                            // before css-loader should be applied
-                            // to @import(ed) resources
                             importLoaders: 1,
                             localsConvention: 'camelCase',
-                            // Create source maps for CSS files
                             sourceMap: true
                         }
                     },
